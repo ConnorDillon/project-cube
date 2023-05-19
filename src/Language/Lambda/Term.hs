316b0a326beac2d5a1393d5b2d5172fc7f2786ad
Connor Dillon <connor@cdillon.nl>
2023-05-19 16:55:01 +0200

    Add alpha and beta reduction

--- src/Language/Lambda/Term.hs
+++ src/Language/Lambda/Term.hs
@@ -1,5 +1,15 @@
-module Language.Lambda.Term ( Lambda(..) ) where
+module Language.Lambda.Term
+  ( Lambda(..)
+  , alphaConvert
+  , alphaPrime
+  , freeVars
+  , subs
+  , betaReduceStep
+  , betaReduce
+  ) where
 
+import Data.Set ( Set )
+import qualified Data.Set as Set
 import Data.Text ( Text )
 import qualified Data.Text as Text
 
@@ -7,10 +17,57 @@ data Lambda
   = Var Text
   | Abs Text Lambda
   | App Lambda Lambda
-  deriving Eq
+  deriving (Eq, Ord)
 
 instance Show Lambda where
   show l = case l of
     Var x -> Text.unpack x
     Abs x y -> concat ["(λ", Text.unpack x, ".", show y, ")"]
     App x y -> concat ["(", show x, " ", show y, ")"]
+
+alphaConvert :: Text -> Lambda -> Maybe Lambda
+alphaConvert new (Abs old term) = Abs new <$> conv old new term
+  where 
+    conv old new term = case term of
+      Var x ->
+        if x == new then Nothing
+        else if x == old then Just (Var new)
+        else Just (Var x)
+      Abs x y ->
+        if x == new then Nothing
+        else if x == old then Just (Abs x y)
+        else Abs x <$> conv old new y
+      App x y -> App <$> conv old new x <*> conv old new y
+alphaConvert _ _ = Nothing
+
+alphaPrime :: Lambda -> Lambda
+alphaPrime l@(Abs x y) = case alphaConvert x' l of
+  Just n -> n
+  Nothing -> alphaPrime $ Abs x' y
+  where x' = Text.snoc x '\''
+alphaPrime l = l
+
+freeVars :: Lambda -> Set Text
+freeVars (Var x) = Set.singleton x
+freeVars (Abs x y) = Set.delete x (freeVars y)
+freeVars (App x y) = Set.union (freeVars x) (freeVars y)
+
+subs :: Text -> Lambda -> Lambda -> Lambda
+subs s r (Var x) = if x == s
+  then r
+  else Var x
+subs s r l@(Abs x y)
+  | x == s = l
+  | Set.member x (freeVars r) = subs s r (alphaPrime l)
+  | otherwise = Abs x $ subs s r y
+subs s r (App x y) = App (subs s r x) (subs s r y)
+  
+betaReduceStep :: Lambda -> Maybe Lambda
+betaReduceStep (App (Abs x y) z) = Just $ subs x z y
+betaReduceStep (App x@(App _ _) y) = do
+  z <- betaReduceStep x
+  return $ App z y
+betaReduceStep _ = Nothing
+
+betaReduce :: Lambda -> Lambda
+betaReduce l = maybe l betaReduce (betaReduceStep l)
--- test/Test.hs
+++ test/Test.hs
@@ -1,16 +1,27 @@
 module Main (main) where
 
-import Language.Lambda ( parse )
+import Language.Lambda.Parser ( parse )
+import Language.Lambda.Term
+  ( Lambda(..), alphaConvert, freeVars, betaReduceStep, betaReduce )
 
 import Data.Text ( Text )
 import qualified Data.Text as Text
 import Data.Void ( Void )
 import Test.Hspec ( hspec, describe, it, shouldBe )
 import Test.Hspec.Megaparsec ( shouldParse )
-import Text.Megaparsec ( ParseErrorBundle )
+import qualified Data.Set as Set
 
 shouldParseTo :: Text -> Text -> IO ()
-shouldParseTo x = (fmap (Text.pack . show) (parse x) `shouldParse`)
+shouldParseTo = shouldParse . fmap (Text.pack . show) . parse
+
+shouldResultIn :: (Eq a,Show a) => (Lambda -> a, Text) -> a -> IO ()
+shouldResultIn (fn, x) = shouldParse $ fmap fn (parse x)
+
+shouldResultIn' :: (Lambda -> Maybe Lambda, Text) -> Text -> IO ()
+shouldResultIn' (fn, x) = shouldResultIn (fmap (Text.pack . show) . fn, x) . Just
+
+shouldReduceTo :: Text -> Text -> IO ()
+shouldReduceTo x = shouldParse (fmap (Text.pack . show . betaReduce) (parse x))
 
 main :: IO ()
 main = hspec $ do
@@ -23,3 +34,16 @@ main = hspec $ do
       "(λx.x) λy.y" `shouldParseTo` "((λx.x) (λy.y))"
     it "handles lambda backslash" $ do
       "\\x.x" `shouldParseTo` "(λx.x)"
+
+  describe "interpreter" $ do
+    it "does alpha conversion" $ do
+      (alphaConvert "y", "λx.x") `shouldResultIn'` "(λy.y)"
+      (alphaConvert "y", "λx.λx.x") `shouldResultIn'` "(λy.(λx.x))"
+    it "finds free vars" $ do
+      (freeVars, "x") `shouldResultIn` Set.singleton "x"
+      (freeVars, "λx.x y") `shouldResultIn` Set.singleton "y"
+    it "does one step beta reduction" $ do
+      (betaReduceStep, "(λx.x) y") `shouldResultIn'` "y"
+      (betaReduceStep, "(λx.x) (λx.x) y") `shouldResultIn'` "((λx.x) y)"
+    it "does many step beta reduction" $ do
+      "(λx.x) (λy.y) z" `shouldReduceTo` "z"
